e87c3089ecdc0a33371615fd5219bfc7a7c6d790
Rizjami Putera <rizjamiputera@gmail.com>
2025-04-21 17:23:54 +0700

    install swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // APP VERSIONING
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  // SWAGGER DOCUMENTATION
+  const config = new DocumentBuilder()
+    .setTitle('Logique - Rizjami Putera')
+    .setDescription('Logique Technical Test -  API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   // RUN APP
   const port = process.env.PORT ?? 5555;
